2078cada415f8bdea137f0bf723a872e3503dfeb
fibuwimo <ray-rem-craim-requiem@ezweb.ne.jp>
2022-03-25 11:58:47 +0900

    HighLowGame追加

---
+++ HighLowGame.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class HighLowGame{
+	public static void main(String[] args){
+		int pcn = new Random().nextInt(99)+1;
+		int count = 0;
+		System.out.println("1から99までの値を1つ生成したよ。");
+		while(true){
+			count++;
+			System.out.print("いくつかな？>>");
+			int pln = new Scanner(System.in).nextInt();
+			if(pcn > pln){
+				System.out.println("もっと上だよ");
+			}else if(pcn < pln){
+				System.out.println("もっと下だよ");
+			}else if(pcn == pln){
+				System.out.println("正解！");
+				break;
+			}
+		}
+		System.out.println(count+"回で正解したよ。");
+	}
+}
